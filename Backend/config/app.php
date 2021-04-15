d54130953fc588427e713d9f633b400b2eb7b36d
Santiago <santiagocordova2997@outlook.es>
2021-04-14 22:44:27 -0500

    url

--- Backend/config/app.php
+++ Backend/config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', '"http://3.138.190.20:3000/api",'),
+    'url' => env('APP_URL', 'http://3.138.190.20:3000/api'),
 
     'asset_url' => env('ASSET_URL', null),
 
